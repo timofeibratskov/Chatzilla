b8dd6f229e926d231ad6f5c1f913287c74376f36
timofei <tima.bratskov05@mail.ru>
2025-03-24 22:37:34 +0300

    add jwtutil and SwaggerConfig

---
+++ backend/chat-service/src/main/java/com/example/chat_service/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.example.chat_service.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@io.swagger.v3.oas.annotations.security.SecurityScheme(
+        name = "BearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+
+)
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Chat Service API")
+                        .version("1.0")
+                        .description("API для чатов с аутентификацией JWT"))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
---
+++ backend/chat-service/src/main/java/com/example/chat_service/util/JwtUtil.java
@@ -0,0 +1,32 @@
+package com.example.chat_service.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+@Component
+@RequiredArgsConstructor
+public class JwtUtil {
+    private final String secretKey = "T5Rsd+2zSxyfWBXpDqmaZG3qULKHANv7pD5VtdgvDz6BXS4y0YVRFeo4zVx8vWbNvrAQ6aCXGUQII/ZosAltbA==";
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey()) // Устанавливаем ключ для проверки подписи
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+    // Метод для получения user_id из токена
+    public String getUserIdFromToken(String token) {
+        Claims claims = extractAllClaims(token);
+        return claims.get("sub", String.class); // Предполагаем, что user_id хранится в токене
+    }
+
+    // Метод для получения секретного ключа
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+}
