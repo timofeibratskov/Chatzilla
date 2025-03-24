f5811c6b374c8a12f55dbf772acdda736ab65eaf
timofei <tima.bratskov05@mail.ru>
2025-03-24 21:23:16 +0300

    add discovery service

---
+++ backend/discovery-serivce/src/main/java/com/example/discovery_serivce/DiscoverySerivceApplication.java
@@ -0,0 +1,13 @@
+package com.example.discovery_serivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverySerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverySerivceApplication.class, args);
+	}
+
+}
---
+++ backend/discovery-serivce/src/test/java/com/example/discovery_serivce/DiscoverySerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discovery_serivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverySerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
