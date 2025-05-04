65bfe9b6bdb0342e92ed45a745f9f229ac0a7a07
timofei <tima.bratskov05@mail.ru>
2025-05-04 14:44:18 +0300

    clients апдейт + новый

--- backend/chat-service/src/main/java/com/example/chat_service/client/UserServiceClient.java
+++ backend/chat-service/src/main/java/com/example/chat_service/client/UserServiceClient.java
@@ -3,21 +3,24 @@ package com.example.chat_service.client;
 import com.example.chat_service.dto.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.List;
 import java.util.UUID;
 
 @FeignClient(name = "user-service")
 public interface UserServiceClient {
+
     @GetMapping("/api/v1/users/{id}")
     UserDto getUserById(
-            @PathVariable UUID id);
+            @PathVariable UUID id,
+            @RequestHeader("Authorization") String authorization);
 
     @PostMapping("/api/v1/users/batch")
     List<UserDto> getUsersBatch(
-            @RequestBody List<UUID> ids
-    );
+            @RequestBody List<UUID> ids,
+            @RequestHeader("Authorization") String authorization);
 }
